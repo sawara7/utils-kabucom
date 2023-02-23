a91b594c692b21886547f0d1fc110dd63e13db8d
sawara7 <sawara7+dev@gmail.com>
2023-02-24 01:25:38 +0900

    commit.

--- dist/lib/type.d.ts
+++ dist/lib/type.d.ts
@@ -1,8 +1,8 @@
 export type stringBoolean = "true" | "false";
 export declare const productTypes: readonly ["0", "1", "2", "3", "4"];
-export type productType = typeof productTypes;
+export type productType = typeof productTypes[number];
 export declare const exchangeCode: readonly [1, 3, 5, 6, 9, 2, 23, 24];
-export type exchangeCodeType = typeof exchangeCode;
+export type exchangeCodeType = typeof exchangeCode[number];
 export declare const orderSides: readonly ["1", "2"];
-export type OrderSide = typeof orderSides;
+export type OrderSide = typeof orderSides[number];
 export type SecurityType = 1;
--- src/lib/type.ts
+++ src/lib/type.ts
@@ -8,7 +8,7 @@ export const productTypes = [
   "3", //先物
   "4"  //OP
 ] as const
-export type productType = typeof productTypes
+export type productType = typeof productTypes[number]
 
 // 市場コード
 export const exchangeCode = [
@@ -21,13 +21,13 @@ export const exchangeCode = [
     23, //日中
     24  //夜間
 ] as const
-export type exchangeCodeType = typeof exchangeCode
+export type exchangeCodeType = typeof exchangeCode[number]
 
 // 売買区分
 export const orderSides = [
   "1",  //売
   "2"   //買
 ] as const
-export type OrderSide = typeof orderSides
+export type OrderSide = typeof orderSides[number]
 
 export type SecurityType = 1
